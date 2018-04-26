Fixed bug when correcting chi2 for upper limit. Previouly error were negative, now there are positive so needed to change the sign in the erf function

pcigale/analysis_modules/pdf_analysis/utils.py
@@ -102,7 +102,7 @@ def dchi2_over_ds2(s, obsdata, obsdata_err, obslim, obslim_err, moddata,
     return func
 
 
-def _compute_scaling(models, obs, wz):
+def _compute_scaling(models, obs, corr_dz, wz):
     """Compute the scaling factor to be applied to the model fluxes to best fit
     the observations. Note that we look over the bands to avoid the creation of
     an array of the same size as the model_fluxes array. Because we loop on the
@@ -115,6 +115,9 @@ def _compute_scaling(models, obs, wz):
     obs: Observation class instance
         Contains the fluxes, intensive properties, extensive properties and
         their errors, for a sigle observation.
+    corr_dz: float
+        Correction factor to scale the extensive properties to the right
+        distance
     wz: slice
         Selection of the models at the redshift of the observation or all the
         redshifts in photometric-redshift mode.
@@ -133,19 +136,17 @@ def _compute_scaling(models, obs, wz):
         # Multiplications are faster than divisions, so we directly use the
         # inverse error
         inv_err2 = 1. / obs.flux_err[band] ** 2.
-        if np.isfinite(flux) and obs.flux_err[band] > 0.:
-            model = models.flux[band][wz]
-            num += model * (flux*inv_err2)
-            denom += model ** 2. * inv_err2
+        model = models.flux[band][wz]
+        num += model * (flux * inv_err2)
+        denom += model ** 2. * inv_err2
 
     for name, prop in obs.extprop.items():
         # Multiplications are faster than divisions, so we directly use the
         # inverse error
         inv_err2 = 1. / obs.extprop_err[name] ** 2.
-        if np.isfinite(prop) and obs.extprop_err[name] > 0.:
-            model = models.extprop[name][wz]
-            num += model * (prop*inv_err2)
-            denom += model ** 2. * inv_err2
+        model = models.extprop[name][wz]
+        num += model * (prop * inv_err2 * corr_dz)
+        denom += model ** 2. * (inv_err2 * corr_dz ** 2.)
 
     return num/denom
 
@@ -235,7 +236,7 @@ def compute_chi2(models, obs, corr_dz, wz, lim_flag):
         scaling of the models to obtain the minimum χ²
     """
     limits = lim_flag and (len(obs.flux_ul) > 0 or len(obs.extprop_ul) > 0)
-    scaling = _compute_scaling(models, obs, wz)
+    scaling = _compute_scaling(models, obs, corr_dz, wz)
 
     # Some observations may not have flux values in some filter(s), but
     # they can have upper limit(s).
@@ -256,7 +257,7 @@ def compute_chi2(models, obs, corr_dz, wz, lim_flag):
     # Computation of the χ² from intensive properties
     for name, prop in obs.intprop.items():
         model = models.intprop[name][wz]
-        chi2 += ((prop - model) * (1. / obs.intprops_err[name])) ** 2.
+        chi2 += ((prop - model) * (1. / obs.intprop_err[name])) ** 2.
 
     # Computation of the χ² from extensive properties
     for name, prop in obs.extprop.items():
@@ -273,14 +274,15 @@ def compute_chi2(models, obs, corr_dz, wz, lim_flag):
             chi2 -= 2. * np.log(.5 *
                                 (1. + erf(((obs.flux_ul[band] -
                                  model * scaling) /
-                                 (-np.sqrt(2.)*obs_error)))))
+                                 (np.sqrt(2.)*obs_error)))))
+
         for band, obs_error in obs.extprop_ul_err.items():
             model = models.extprop[band][wz]
             chi2 -= 2. * np.log(.5 *
                                 (1. + erf(((obs.extprop_ul[band] -
                                  model * scaling) /
-                                 (-np.sqrt(2.)*obs_error)))))
-
+                                 (np.sqrt(2.)*obs_error)))))
+ 
     return chi2, scaling
 
 